759926e87a52b2923d829e7a869aeb529696da7b
mraysu <mike.ray.sullivan@gmail.com>
2025-12-16 16:01:50 -0800

    dev scripts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,14 @@
 import type { NextConfig } from "next";
-import type { Configuration } from "webpack";
 
 const nextConfig: NextConfig = {
-  webpack(config: Configuration): Configuration {
-    config.module?.rules?.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
   },
-  output: "export",
   images: {
     unoptimized: true,
   },
