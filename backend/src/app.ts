bd8aa91ca5a6e008bdf808901b37650d66feb042
thomas-rocha <trocha@ucsd.edu>
2026-01-19 23:58:23 -0800

    added base program and programs routes to api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,9 @@ import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 
+import programRoutes from "../src/routes/program";
+import programsRoutes from "../src/routes/programs";
+
 import { FRONTEND_ORIGIN, MONGO_URI, PORT } from "./config";
 import errorHandler from "./middleware/errorHandler";
 import log from "./middleware/logger";
@@ -14,6 +17,9 @@ app.use(
     methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
   }),
 );
+
+app.use("/programs", programsRoutes);
+app.use("/program", programRoutes);
 
 app.use(express.json());
 
--- backend/src/controllers/program.ts
+++ backend/src/controllers/program.ts
@@ -1,7 +1,3 @@
-/**
- * Functions that process task route requests.
- */
-
 import createHttpError from "http-errors";
 
 import ProgramModel from "../../src/models/program";
@@ -11,12 +7,10 @@ import type { RequestHandler } from "express";
 /**
  * Get a program by its ID
  * @param req The Request object from Express. This contains all the data from
- * the API request. (https://expressjs.com/en/4x/api.html#req)
+ * the API request
  * @param res The Response object from Express. We use this to generate the API
- * response for Express to send back. (https://expressjs.com/en/4x/api.html#res)
- * @param next The next function in the chain of middleware. If there's no more
- * processing we can do in this handler, but we're not completely done handling
- * the request, then we can pass it along by calling next().
+ * response for Express to send back
+ * @param next The next function in the chain of middleware
  */
 export const getProgram: RequestHandler = async (req, res, next) => {
   const { id } = req.params;
