b8ccc548215ac7c9e3881ac11a175bd306bd5ee1
Azpi <jesusazpitartedoesart@gmail.com>
2026-01-31 22:31:37 -0800

    feature: added TSE constellation support

--- frontend/src/app/(pages)/test/page.tsx
+++ frontend/src/app/(pages)/test/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+import { Button } from "@tritonse/tse-constellation";
+
+/* 
+All constellation components can be found here:
+https://tritonse.github.io/TSE-Constellation/?path=/docs/welcome--documentation
+*/
 export default function Test() {
-  return <div>This Test Page is meant for developers and will be removed in the MVP.</div>;
+  return (
+    <div>
+      This Test Page is meant for developers and will be removed in the MVP.
+      <br />
+      <div style={{ display: "flex", gap: "8px" }}>
+        <Button> Button </Button>
+        <Button variant="secondary"> Secondary Button </Button>
+        <Button variant="tag"> Tag Variant Button </Button>
+        <Button leadingIcon="ic_upload"> Leading Icon Button</Button>
+        <Button trailingIcon="ic_upload"> Trailing Button</Button>
+        <Button disabled> Disabled Button </Button>
+        <Button small> Small Button </Button>
+        <Button destructive> Small Button </Button>
+      </div>
+    </div>
+  );
 }
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,12 @@
-import { Navbar } from "./(ui)/_components/Navbar/Navbar";
+"use client";
+
 import "./globals.css";
+import { defaultTheme, ThemeProvider } from "@tritonse/tse-constellation";
+import Head from "next/head";
 
-import type { Metadata } from "next";
+import { Navbar } from "./(ui)/_components/Navbar/Navbar";
 
-export const metadata: Metadata = {
+const metadata = {
   title: "Meemli",
   description: "A Next.js app with the new app router",
 };
@@ -11,11 +14,35 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>
-        <header>
-          <Navbar />
-        </header>
-        <main>{children}</main>
+        <ThemeProvider
+          colors={{
+            ...defaultTheme.colors,
+
+            /** Primary colors */
+            primary_light: "#FFFFFF",
+            primary_dark: "#102D5F", // Accent blue
+
+            // /** Functional colors */
+            background: "#ECEFF3",
+            success: "#3BB966",
+            error: "#BE2D46",
+            disabled: "#818181",
+
+            // /** Neutral colors */
+            black: "#232220",
+          }}
+        >
+          <header>
+            <Navbar />
+          </header>
+          <main>{children}</main>
+        </ThemeProvider>
       </body>
     </html>
   );
