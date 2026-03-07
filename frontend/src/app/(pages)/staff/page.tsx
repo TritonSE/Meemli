7cad1b72e5dfdfc774773048c11cd7b5925fbf60
OrigamiStarz <40199724+OrigamiStarz@users.noreply.github.com>
2026-03-06 17:08:49 -0800

    Delete page component

--- frontend/src/app/(pages)/staff/page.tsx
+++ frontend/src/app/(pages)/staff/page.tsx
@@ -5,27 +5,24 @@ import { useState } from "react";
 import { AddStaffForm } from "@/src/components/AddStaff/AddStaffForm";
 import { Button } from "@/src/components/Button";
 import { Modal } from "@/src/components/Modal";
-import { Page } from "@/src/components/Page";
 
 export default function Staff() {
   const [addOpen, setAddOpen] = useState(false);
 
   return (
-    <Page>
-      <section>
-        <h1>Staff</h1>
-        <Button label="Add Staff" kind="primary" onClick={() => setAddOpen(!addOpen)} />
-        {addOpen && (
-          <Modal
-            onExit={() => setAddOpen(!addOpen)}
-            child={
-              <>
-                <AddStaffForm onExit={() => setAddOpen(false)} />
-              </>
-            }
-          />
-        )}
-      </section>
-    </Page>
+    <section>
+      <h1>Staff</h1>
+      <Button label="Add Staff" kind="primary" onClick={() => setAddOpen(!addOpen)} />
+      {addOpen && (
+        <Modal
+          onExit={() => setAddOpen(!addOpen)}
+          child={
+            <>
+              <AddStaffForm onExit={() => setAddOpen(false)} />
+            </>
+          }
+        />
+      )}
+    </section>
   );
 }
