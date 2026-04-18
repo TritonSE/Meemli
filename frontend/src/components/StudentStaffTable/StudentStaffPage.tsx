357799c34a638a07a98457802a4da7fc18fe6b32
thomas-rocha <trocha@ucsd.edu>
2026-04-18 15:28:07 -0700

    improved search capabilities

--- frontend/src/components/StudentStaffTable/StudentStaffPage.tsx
+++ frontend/src/components/StudentStaffTable/StudentStaffPage.tsx
@@ -127,16 +127,16 @@ export default function StudentStaffPage({ type, state, disabled }: StudentStaff
       // filter by search query if it exists. Checks for matches in display name and email.
       if (q) {
         const terms = q.split(" ");
-        base = base.filter(
-          (s) =>
-            terms.every((term) =>
+        base = base.filter((s) =>
+          terms.every(
+            (term) =>
               (s.displayName ?? "").toLowerCase().includes(term) ||
               (s.meemliEmail ?? "").toLowerCase().includes(term) ||
               (s.parentContact?.firstName ?? "").toLowerCase().includes(term) ||
               (s.parentContact?.lastName ?? "").toLowerCase().includes(term) ||
               (s.parentContact?.email ?? "").toLowerCase().includes(term) ||
               (s.comments ?? "").toLowerCase().includes(term),
-            )
+          ),
         );
       }
 
@@ -159,13 +159,13 @@ export default function StudentStaffPage({ type, state, disabled }: StudentStaff
 
       if (q) {
         const terms = q.split(" ");
-        base = base.filter(
-          (s) =>
-            terms.every((term) =>
+        base = base.filter((s) =>
+          terms.every(
+            (term) =>
               (s.firstName ?? "").toLowerCase().includes(term) ||
               (s.lastName ?? "").toLowerCase().includes(term) ||
               (s.meemliEmail ?? "").toLowerCase().includes(term),
-            )
+          ),
         );
       }
 
